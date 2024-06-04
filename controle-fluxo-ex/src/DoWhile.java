7c21013ca07284f73b2881b8e61de0355a896e18
Filipe <162770790+filipedhunior@users.noreply.github.com>
2024-06-04 16:26:43 -0300

    [Add] Exercicios sobre While e DoWhile

---
+++ controle-fluxo-ex/src/DoWhile.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando....");
+        do {
+            // executando repetidas vezes até alguem atender
+            System.out.println("Telefone tocando");    
+
+        } while (tocando());
+
+        System.out.println("Alo !!!!");
+    }
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        // negando o ato de continuar tocando
+        return ! atendeu;
+}
+}
