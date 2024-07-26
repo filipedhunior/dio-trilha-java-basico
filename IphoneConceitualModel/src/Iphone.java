b4a307d2dbe01c231310a4f057ba29869a668e51
Filipe Dhunior <designer.filipedhunior@gmail.com>
2024-07-26 14:09:09 -0300

    [Add] Iphone Java Class for Reprodutor
    Classes base do iphone

--- IphoneConceitualModel/src/Iphone.java
+++ IphoneConceitualModel/src/Iphone.java
@@ -1,6 +1,9 @@
 public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
 
     //TODO: Implementar metodos de reprodutor
+        public void tocar(){}
+        public void pausar(){}
+        public void selecionarMusica(String musica){}
     // TODO: Implementar metodos de reprodutor
     // TODO: Implementar metodos de reprodutor
 }
