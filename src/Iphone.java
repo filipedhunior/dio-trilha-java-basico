1d9f0ce3aa1081d0104cad0e66d41743b49d95f8
Filipe Dhunior <designer.filipedhunior@gmail.com>
2024-07-26 13:46:38 -0300

    [Add] Methods in Iphone

--- src/Iphone.java
+++ src/Iphone.java
@@ -1,2 +1,15 @@
 public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+    // REPRODUTOR MUSICAL
+    public void tocar(){
+        System.out.println("Tocando musicas no Aplle Music do Iphone");
+    }
+    public void pausar(){
+        System.out.println("Musica pausada no Apple Music do Iphone");
+    }
+    public void selecionarMusica(String musica){
+        System.out.println(musica +" selecionada no Apple Music do Iphone");
+
+    }
+
+    // APARELHO TELEFONICO
 }
